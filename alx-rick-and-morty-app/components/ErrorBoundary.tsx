9df38bf9bc6d119ea2403f2b87369cc8041de4b5
veenzent <odumevincent19@gmail.com>
2025-12-17 12:54:45 +0100

    Task 3. Monitor and Log Errors

--- alx-rick-and-morty-app/components/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from "react";
+import * as Sentry from '@sentry/react';
 
 interface State {
     hasError: boolean;
@@ -19,7 +20,7 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
     }
 
     componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-        console.log({ error, errorInfo });
+        Sentry.captureException(error, { extra: errorInfo });
     }
 
     render() {
